c5b00be551f1968280aa63691d8b783020554e7b
MimakiShuhei <284925179+MimakiShuhei@users.noreply.github.com>
2026-06-13 16:40:42 +0900

    エラー修正

--- backend/src/api/models/message.ts
+++ backend/src/api/models/message.ts
@@ -1,2 +1,6 @@
-export type MessageRequest = {thread: string; message: string; userName: string; password: string };
-
+export type MessageRequest = {
+  thread: string;
+  message: string;
+  userName: string;
+  password: string;
+};
